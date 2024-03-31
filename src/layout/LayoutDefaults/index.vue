01ee572a973ac73c364e98c9071062fc71200db6
wang_fan_w <2547096351@qq.com>
2024-03-31 18:12:33 +0800

    feat: 全局变量规范

--- src/layout/LayoutDefaults/index.vue
+++ src/layout/LayoutDefaults/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>默认布局</div>
+  <a-layout class="layout">
+    <a-layout-sider>Sider</a-layout-sider>
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+    </a-layout>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineOptions({ name: "LayoutDefaults" });
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout {
+  height: 100vh;
+}
+</style>
