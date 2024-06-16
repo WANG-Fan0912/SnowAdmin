55eef8ff66f3b81a60ebf36549e5546707a76b2f
wang_fan_w <2547096351@qq.com>
2024-06-16 23:25:51 +0800

    feat: 国际化预览

--- src/views/internationalization/internationalization.vue
+++ src/views/internationalization/internationalization.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="dc-page">
-    <a-date-picker style="width: 200px" />
-    <a-pagination :total="50" show-total show-jumper show-page-size />
+    <p>请切换语言来预览国际化效果</p>
+    <br />
+    <div>
+      <a-date-picker style="width: 200px" />
+    </div>
+    <br />
+    <div>
+      <a-time-picker type="time-range" style="width: 252px" />
+    </div>
+    <br />
+    <div>
+      <a-range-picker
+        style="width: 360px"
+        show-time
+        :time-picker-props="{ defaultValue: ['00:00:00', '09:09:06'] }"
+        format="YYYY-MM-DD HH:mm"
+      />
+    </div>
+    <br />
+    <div>
+      <a-pagination :total="50" show-total show-jumper show-page-size />
+    </div>
   </div>
 </template>
 
