7dce737aed8b3b198068ba7b8ccb323e62b9d0f9
wf <2547096351@qq.com>
2024-06-20 18:27:51 +0800

    feat: 用户设置

--- src/views/personal-center/user-settings/user-settings.vue
+++ src/views/personal-center/user-settings/user-settings.vue
@@ -1,7 +1,25 @@
 <template>
-  <div class="dc-page">用户设置</div>
+  <div class="dc-page">
+    <a-row>
+      <a-col :span="4">
+        <div>
+          <a-avatar :size="100">
+            <img alt="avatar" :src="myImage" />
+          </a-avatar>
+        </div>
+      </a-col>
+      <a-col :span="6">
+        <div>头像</div>
+      </a-col>
+      <a-col :span="6">
+        <div>头像</div>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import myImage from "@/assets/img/my-image.jpg";
+</script>
 
 <style lang="scss" scoped></style>
