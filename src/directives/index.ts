0af361ea04a2f5f8b762011c9d814343a7ef6719
wf <2547096351@qq.com>
2024-07-16 18:49:19 +0800

    little: 一点小修改

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,11 +1,13 @@
+import { App } from "vue";
 import antiShake from "@/directives/modules/anti-shake";
 import throttle from "@/directives/modules/throttle";
+
 // 定义安装函数
 // install 函数是一个对象中的方法，其作用是将一系列指令对象安装到 Vue 应用实例中，它自带两个参数：app 和 options
 // app就是vue实例，options则是安装函数的参数(可选)
 // install 方法的定义就插件对象，install 方法会在引入插件时自动被 vue 调用，并传参 vue 实例和 options
 const directives = {
-  install(app: any) {
+  install(app: App<Element>) {
     // 将一系列自定义指令对象安装到 Vue 应用实例中
     app.directive("antiShake", antiShake);
     app.directive("throttle", throttle);
