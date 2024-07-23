7294244d7d55623b4ff7e2be2249d9a564c07c46
wf <2547096351@qq.com>
2024-07-23 19:32:08 +0800

    little: 测试指令

--- src/directives/modules/custom.ts
+++ src/directives/modules/custom.ts
@@ -1,8 +1,15 @@
 import { Directive } from "vue";
 
 const custom: Directive = {
-  mounted(el, binding) {
-    el.style.color = binding.value;
+  mounted(el, binding, vnode) {
+    el.__onClick__ = () => {
+      binding.value(vnode.props);
+    };
+    el.addEventListener("click", el.__onClick__);
+  },
+  beforeUnmount(el) {
+    // 解绑dom上的点击事件
+    el.removeEventListener("click", el.__onClick__);
   }
 };
 
--- src/views/custom-instruction/test-instruction/test-instruction.vue
+++ src/views/custom-instruction/test-instruction/test-instruction.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="dc-page">
-    <div v-custom="'red'">文字变色</div>
+    <button v-custom="getFun" :goodsId="goodsId">文字变色</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const goodsId = ref(100);
+setTimeout(() => {
+  goodsId.value = 200;
+}, 1000);
+
+const getFun = (e: any) => {
+  console.log("点击事件", e);
+};
+</script>
 
 <style lang="scss" scoped></style>
