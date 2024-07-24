324a241319f8fdb739963657ddf63989a7287adf
wf <2547096351@qq.com>
2024-07-24 18:22:06 +0800

    little: 未做更改

--- src/directives/modules/custom.ts
+++ src/directives/modules/custom.ts
@@ -3,13 +3,10 @@ import { Directive } from "vue";
 const custom: Directive = {
   mounted(el, binding, vnode) {
     el.__onClick__ = () => {
-      binding.value(vnode.props);
+      let { goodsId, event } = binding.value;
+      event(goodsId);
     };
     el.addEventListener("click", el.__onClick__);
-  },
-  beforeUnmount(el) {
-    // 解绑dom上的点击事件
-    el.removeEventListener("click", el.__onClick__);
   }
 };
 
--- src/views/custom-instruction/test-instruction/test-instruction.vue
+++ src/views/custom-instruction/test-instruction/test-instruction.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="dc-page">
-    <button v-custom="getFun" :goodsId="goodsId">文字变色</button>
+    测试指令
+    <!-- <button v-custom="{ goodsId, event: getFun }" :rowData="goodsId">文字变色</button> -->
   </div>
 </template>
 
 <script setup lang="ts">
-const goodsId = ref(100);
-setTimeout(() => {
-  goodsId.value = 200;
-}, 1000);
+// const goodsId = ref(100);
+// setTimeout(() => {
+//   goodsId.value = 200;
+// }, 1000);
 
-const getFun = (e: any) => {
-  console.log("点击事件", e);
-};
+// setTimeout(() => {
+//   goodsId.value = 300;
+// }, 3000);
+
+// setTimeout(() => {
+//   goodsId.value = 400;
+// }, 4000);
+// const getFun = (e: any) => {
+//   console.log("点击事件", e);
+// };
 </script>
 
 <style lang="scss" scoped></style>
