b66ee87cfff72feaad55d4f5026588b46b61b9f4
WANGFAN\wangf <15871339963@163.com>
2024-12-05 17:28:39 +0800

    test测试提交

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 import { loadingPage } from "@/utils/loading-page";
 import { useThemeMethods } from "@/hooks/useThemeMethods";
 
-let a = "123456789";
+let a = "123";
 
 // 初始化主题
 const onTheme = () => {
