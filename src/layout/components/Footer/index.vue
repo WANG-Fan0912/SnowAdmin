855df5a12663ceb6397f1c00c4b25302c4cd8b98
liujiayii <liujiayii@foxmail.com>
2025-06-13 18:21:25 +0800

    fix(layout-footer): 修复正确的年份

--- src/layout/components/Footer/index.vue
+++ src/layout/components/Footer/index.vue
@@ -1,10 +1,14 @@
 <template>
   <a-layout-footer class="footer">
-    <div class="footer_title" @click="onFooter">2024 © SnowAdmin by 兔子先森</div>
+    <div class="footer_title" @click="onFooter">{{ currentYear }} © SnowAdmin by 兔子先森</div>
   </a-layout-footer>
 </template>
 
 <script setup lang="ts">
+import dayjs from "dayjs"
+
+const currentYear = dayjs().format("YYYY")
+
 const onFooter = () => {
   window.open("https://github.com/WANG-Fan0912/SnowAdmin", "_blank");
 };
